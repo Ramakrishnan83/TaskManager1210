11bb4fc9cbe64161e3e73f225c842374f348d1f5
Ramakrishnan83 <45376705+Ramakrishnan83@users.noreply.github.com>
2018-12-10 14:45:36 -0500

    Delete database.js

--- database.js
+++
@@ -1,9 +0,0 @@
-var mongoose=require('mongoose');
-mongoose.connect('mongodb://localhost:27017/task_manager', { useNewUrlParser: true }, function(err){
-    if(!!err){
-        console.log("not able to connection");
-        console.error(err);
-    } else {
-        console.log("Connection established successfully..");
-    }
-});
